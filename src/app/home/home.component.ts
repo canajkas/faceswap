df2c3f42be7e450859c2c2b63ddef67123eaa7ca
Kastriot Canaj <75947351+canajkas@users.noreply.github.com>
2024-12-19 13:10:03 +0000

    Test2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  basePhotoUploaded = false;
 
+  triggerFileInput() {
+    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+    fileInput.click();
+  }
+
+  handleFileInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.saveFile(file);
+      this.basePhotoUploaded = true;
+    }
+  }
+
+  saveFile(file: File) {
+    // Hier kannst du die Logik zum Speichern der Datei implementieren
+    console.log('File uploaded:', file);
+  }
 }
