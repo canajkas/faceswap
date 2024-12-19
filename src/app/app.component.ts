0107c4ccbf85b7da3abfc1472074c1c0107905e0
Kastriot Canaj <75947351+canajkas@users.noreply.github.com>
2024-12-19 13:02:35 +0000

    Work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
+import { LibraryComponent } from './library/library.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterModule, RouterOutlet, HomeComponent, NavigationComponent, AboutComponent, UserComponent, LibraryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
